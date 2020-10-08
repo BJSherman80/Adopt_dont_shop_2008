235a5ee7635150a918e28e858602d0730c984ee6
Brett Turing <bjsherman80@gmail.com>
2020-10-07 20:13:54 -0600

    created new and create action for shelter along w/ form

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -4,4 +4,22 @@ class SheltersController < ApplicationController
     @shelters = Shelter.all
   end
 
+  def show
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    shelter = Shelter.new({
+                            name: params[:shelter][:name],
+                            address: params[:shelter][:address],
+                            city: params[:shelter][:city],
+                            state: params[:shelter][:state],
+                            zip: params[:shelter][:zip]
+                            })
+    shelter.save
+    redirect_to '/shelters'
+  end
 end
