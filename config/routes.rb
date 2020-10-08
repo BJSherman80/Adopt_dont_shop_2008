9d87740e472ba1f89e33f38e58ed49f5e270152b
Brett Turing <bjsherman80@gmail.com>
2020-10-07 20:14:12 -0600

    created new and and post route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get '/shelters', to: 'shelters#index'
+  get '/shelters/new', to: 'shelters#new'
+  get '/shelters/:id', to: 'shelters#show'
+  post '/shelters', to: 'shelters#create'
 end
