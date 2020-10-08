ad7721681a27f4c184fe3a7c7288ab85e9b902f9
Brett Turing <bjsherman80@gmail.com>
2020-10-07 20:38:29 -0600

    created edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get '/shelters/new', to: 'shelters#new'
   get '/shelters/:id', to: 'shelters#show'
   post '/shelters', to: 'shelters#create'
+  patch '/shelters/:id', to: 'shelters#update'
+  get '/shelters/:id/edit', to: 'shelters#edit'
 end
