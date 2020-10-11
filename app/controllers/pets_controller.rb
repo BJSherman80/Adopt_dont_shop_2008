4141c159aa7d02a711ced1327dd12ae9a6ab7a9a
Brett Turing <bjsherman80@gmail.com>
2020-10-11 14:21:06 -0600

    Create pet form,route and action

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,4 +7,23 @@ class PetsController < ApplicationController
   def show
     @pet = Pet.find(params[:id])
   end
+
+  def new
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def create
+    @shelter = Shelter.find(params[:id])
+    pet = @shelter.pets.new(pet_params)
+    pet.save
+    redirect_to "/shelters/#{@shelter.id}/pets"
+  end
+
+  private
+
+  def pet_params
+    params.permit(:name,:age,:sex,:image,:description)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   get '/pets', to: 'pets#index'
   get '/shelters/:id/pets', to: 'shelterpets#index'
   get '/pets/:id', to: 'pets#show'
+  get '/shelters/:id/pets/new', to: 'pets#new'
+  post '/shelters/:id/pets', to: 'pets#create'
 
 end
