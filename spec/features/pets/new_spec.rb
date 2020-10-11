193db393fb23abd40a1133b453b6bae3c1051153
Brett Turing <bjsherman80@gmail.com>
2020-10-11 14:23:56 -0600

    created test for new pet

---
+++ spec/features/pets/new_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "Create new pet ", type: :feature do
+
+  it "can create pet" do
+    shelter_1 = Shelter.create( name: "Sherms Spikey Friends",
+                                address: "1489 Balake Ave.",
+                                city: "Denver",
+                                state: "CO",
+                                zip: "80201",
+
+                                )
+    vernon = shelter_1.pets.create!(name: "Vernon", age: 18, sex: "male", image: "vernon.png" )
+
+
+    visit "/shelters/#{shelter_1.id}/pets"
+    click_on "Create Pet"
+    fill_in :name, with: "Clifford"
+    fill_in :image, with: "https://cdn3-www.dogtime.com/assets/uploads/gallery/chow-chow-dog-breed-pictures/3-fullbody.jpg"
+    fill_in :age, with: 1
+    fill_in :sex, with: "Female"
+    fill_in :sex, with: "Big and fluffy"
+    click_on "Create Pet"
+    expect(current_path).to eq("/shelters/#{shelter_1.id}/pets")
+    expect(page).to have_content("Vernon")
+    expect(page).to have_content("Clifford")
+  end
+end
