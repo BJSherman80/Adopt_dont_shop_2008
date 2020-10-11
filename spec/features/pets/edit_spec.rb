0570c28f3ddbe241bec1bbfd1979aa2f79f4a0c1
Brett Turing <bjsherman80@gmail.com>
2020-10-11 15:51:00 -0600

    added test tp edit pet

---
+++ spec/features/pets/edit_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "pets edit page", type: :feature do
+
+  it "can update pet" do
+    shelter_1 = Shelter.create( name: "Sherms Spikey Friends",
+                                address: "1489 Balake Ave.",
+                                city: "Denver",
+                                state: "CO",
+                                zip: "80201",
+
+                                )
+    vernon = shelter_1.pets.create!(name: "Vernon", age: 18, sex: "male", image: "vernon.png" )
+
+
+    visit "/pets/#{vernon.id}"
+    click_on "Update Pet"
+    fill_in :name, with: "Sammy"
+    expect(page).to have_button("Submit")
+    click_on "Submit"
+    save_and_open_page
+    expect(page).to have_content("Sammy")
+    expect(page).to have_content("male")#make sure the unchanged values still exist
+  end
+end
