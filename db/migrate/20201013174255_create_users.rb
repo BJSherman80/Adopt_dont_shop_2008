e9f8b3bac6a76197da9db31cc7161bcf1829497d
Brett Turing <bjsherman80@gmail.com>
2020-10-13 11:46:21 -0600

    created model test and migrations for user

---
+++ db/migrate/20201013174255_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.string :state
+      t.integer :zip
+
+      t.timestamps
+    end
+  end
+end
