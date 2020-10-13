ca21eb235d356e2c7cc150ba889f2461d3bb24c4
Brett Turing <bjsherman80@gmail.com>
2020-10-13 17:19:09 -0600

    created new user action view and route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    user = User.create(user_params)
+    
+    redirect_to "/users/#{user.id}"
+  end
+
+
+    private
+
+    def user_params
+      params.permit(:name,:address,:city,:state,:zip)
+    end
+
 end
