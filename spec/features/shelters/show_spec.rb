50eaf615093da3ef9eda9724dd1cfa5169513599
Brett Turing <bjsherman80@gmail.com>
2020-10-14 16:58:49 -0600

    crearted edit review test

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -78,4 +78,29 @@ RSpec.describe "shelters show page", type: :feature do
     expect(page).to have_content("This place sucks!")
     expect(page).to have_content("Calvin")
   end
+
+  it 'can edit a review' do
+    user_1 = User.create( name: "Austin Powers",
+                              address: "4555 Shag Ave",
+                              city: "Denver",
+                              state: "CO",
+                              zip: 84444,
+                              )
+    shelter_1 = Shelter.create( name: "Sherms Spikey Friends",
+                                address: "1489 Balake Ave.",
+                                city: "Denver",
+                                state: "CO",
+                                zip: "80201",
+                                )
+
+
+    review = Review.create!(shelter_id: shelter_1.id, user_id: user_1.id, title: "Horrible service", rating: 1, content: "I saw a man slap a kitten", picture: "https://felineengineering.com/wp-content/uploads/2019/07/Adorable-sad-kitten-e1562788887775-974x1024.jpg", name_of_user: "Dr. Evil")
+    visit "/shelters/#{shelter_1.id}"
+      within("#review-#{review.id}") do
+        click_on "Edit Review"
+      end
+      fill_in :title, with: "Extremely Horrible Crap Service"
+      click_on "Update Review"
+      expect(page).to have_content("Extremely Horrible Crap Service")
+  end
 end
