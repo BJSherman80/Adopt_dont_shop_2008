6ce95e145f7ae361747966ba1e72446be05e6c51
Hanna Davis <hanna.e.davis@gmail.com>
2020-10-15 13:29:12 -0600

    created test for incomplete form

--- spec/features/shelters/new_spec.rb
+++ spec/features/shelters/new_spec.rb
@@ -24,4 +24,17 @@ RSpec.describe "new shelter", type: :feature do
     expect(page).to have_content("Burts Farm Animals")
 
   end
+
+  it "incomplete form sends a flash message" do 
+    visit "/shelters/new"
+
+    fill_in :name, with: ""
+    fill_in :address, with: "87453 Dirt Road #4 "
+    fill_in :city, with: "Longmont"
+    fill_in :state, with: "CO"
+    fill_in :zip, with: "897655"
+    click_button "Create Shelter"
+    
+    expect(page).to have_content("Please fill out all fields to update review.")
+  end
 end
