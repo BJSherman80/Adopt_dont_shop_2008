0fdd0811e557345866dc4aefcde9c9e21e524bc4
Brett Turing <bjsherman80@gmail.com>
2020-10-15 19:28:44 -0600

    fixed spec for valid user id

--- spec/features/reviews/incomplete_spec.rb
+++ spec/features/reviews/incomplete_spec.rb
@@ -14,10 +14,10 @@ RSpec.describe "flash notice when form is incomplete", type: :feature do
                                 state: "CO",
                                 zip: "80201",
                                 )
-    review = Review.create!(shelter_id: shelter_1.id, 
-                            user_id: user_1.id, title: "Horrible service", 
-                            rating: 1, content: "I saw a man slap a kitten", 
-                            picture: "https://felineengineering.com/wp-content/uploads/2019/07/Adorable-sad-kitten-e1562788887775-974x1024.jpg", 
+    review = Review.create!(shelter_id: shelter_1.id,
+                            user_id: user_1.id, title: "Horrible service",
+                            rating: 1, content: "I saw a man slap a kitten",
+                            picture: "https://felineengineering.com/wp-content/uploads/2019/07/Adorable-sad-kitten-e1562788887775-974x1024.jpg",
                             name_of_user: "Dr. Evil")
     visit "/shelters/#{shelter_1.id}"
       within("#review-#{review.id}") do
@@ -32,4 +32,36 @@ RSpec.describe "flash notice when form is incomplete", type: :feature do
 
       expect(page).to have_content("Please fill out all fields to update review.")
   end
+
+  it 'will display a flash message if the user does not exist' do
+    user_1 = User.create( name: "Austin Powers",
+                              address: "4555 Shag Ave",
+                              city: "Denver",
+                              state: "CO",
+                              zip: 84444,
+                              )
+    shelter_1 = Shelter.create( name: "Sherms Spikey Friends",
+                                address: "1489 Balake Ave.",
+                                city: "Denver",
+                                state: "CO",
+                                zip: "80201",
+                                )
+    review = Review.create!(shelter_id: shelter_1.id,
+                            user_id: user_1.id, title: "Horrible service",
+                            rating: 1, content: "I saw a man slap a kitten",
+                            picture: "https://felineengineering.com/wp-content/uploads/2019/07/Adorable-sad-kitten-e1562788887775-974x1024.jpg",
+                            name_of_user: "Dr. Evil")
+   visit "/shelters/#{shelter_1.id}"
+
+    click_on "New Review"
+
+     fill_in :title, with: "Extremely Horrible Crap Service"
+     fill_in :rating, with: 0
+     fill_in :content, with: "The goldfish were fighting"
+     fill_in :name_of_user, with: "Cruella Deville"
+
+     click_on "Create Review"
+
+     expect(page).to have_content("User does not exist. Please enter a valid username.")
+ end
 end
