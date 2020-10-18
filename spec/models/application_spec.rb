5c1f07682cd621f3adf568e0568c24d9f6d3a66a
Hanna Davis <hanna.e.davis@gmail.com>
2020-10-18 16:36:10 -0600

    Removed description requirement

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,8 +5,6 @@ describe Application, type: :model do
     describe "validations" do
       it { should validate_presence_of :name_of_user }
       it { should validate_presence_of :address}
-      it { should validate_presence_of :description }
-      it { should validate_presence_of :pets }
       it { should validate_presence_of :status }
     end
 
