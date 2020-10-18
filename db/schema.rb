9f5f785925f1f0275a4ca9425ca7b42ee73689da
Hanna Davis <hanna.e.davis@gmail.com>
2020-10-18 14:28:09 -0600

    Add schema for remove pets column migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_18_173156) do
+ActiveRecord::Schema.define(version: 2020_10_18_201420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2020_10_18_173156) do
     t.string "name_of_user"
     t.string "address"
     t.string "description"
-    t.string "pets"
     t.string "status"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_applications_on_user_id"
