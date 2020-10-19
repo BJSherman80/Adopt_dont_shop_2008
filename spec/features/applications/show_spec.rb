cdf4d7f8ee16e3c0f1a4f2a31e9141170acb75ee
Hanna Davis <hanna.e.davis@gmail.com>
2020-10-18 20:42:04 -0600

    Add test for pet search application feature

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,33 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe 'Application show page', type: :feature do
-  it 'can see application info' do
-    user_1 = User.create!(name: "Dr. Evil",
+  before(:each) do
+    @user_1 = User.create!(name: "Dr. Evil",
                           address: "56774 FLower Ave.",
                           city: "Smallville",
                           state: "Alaska",
                           zip: 87645)
-    shelter_1 = Shelter.create( name: "Brett's Pet Palace",
+    @shelter_1 = Shelter.create( name: "Brett's Pet Palace",
                                 address: "456 Sesame Ave",
                                 city: "Denver",
                                 state: "CO",
                                 zip: 80222)
-    pet_1 = shelter_1.pets.create!(name: "Vernon", 
+    @pet_1 = @shelter_1.pets.create!(name: "Vernon", 
                                     age: 18, 
                                     sex: "male", 
                                     image: "vernon.png")
-    application = user_1.applications.create!(name_of_user: "Dr. Evil",
+    @application = @user_1.applications.create!(name_of_user: "Dr. Evil",
                                       address: "56774 FLower Ave.",
                                       description: "I love this hairless cat",
-                                      pets: [pet_1],
-                                      status: "pending")
+                                      pets: [@pet_1],
+                                      status: "In Progress")
+    end 
 
-      visit "applications/#{application.id}"
+    it 'can see application info' do
+      visit "applications/#{@application.id}"
 
-      expect(page).to have_content(application.name_of_user)
-      expect(page).to have_content(application.address)
-      expect(page).to have_content(application.description)
-      expect(page).to have_content(pet_1.name)
-      expect(page).to have_content(application.status)
+      expect(page).to have_content(@application.name_of_user)
+      expect(page).to have_content(@application.address)
+      expect(page).to have_content(@application.description)
+      expect(page).to have_content(@pet_1.name)
+      expect(page).to have_content(@application.status)
+    end
+
+    it 'search for a pet to add to the application' do
+      visit "/applications/#{@application.id}"
+      expect(page).to have_content("In Progress")
+      fill_in :search, with: "Vernon"
+      click_button 'Search'
+      expect(current_path).to eq("/applications/#{@application.id}")
+      expect(page).to have_content(@pet_1.name)
     end
   end
