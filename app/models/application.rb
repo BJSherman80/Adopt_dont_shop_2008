d23315c4728770e191df1524e8284e1b612da089
Brett Turing <bjsherman80@gmail.com>
2020-10-20 15:38:43 -0600

    refactored method into update actions

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,14 +4,14 @@ class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
 
-    def update_app_status
-      if pets.all{|pet| pet.status == "Approved"}
-        self.update(status: "Approved")
-      elsif pets.any?{|pet| pet.status == "Rejected"}
-        self.update(status: "Rejected")
-      else
-        self.update(status: "In Progress")
-      end
-    end
+    # def update_app_status
+    #   if pets.any?{|pet| pet.status == "Rejected"}
+    #     self.update(status: "Rejected")
+    #   elsif pets.all{|pet| pet.status == "Approved"}
+    #     self.update(status: "Approved")
+    #   else
+    #     self.update(status: "In Progress")
+    #   end
+    # end
 
 end
