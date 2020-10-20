8909d0c06592840d9a13f9935535425054f6af6b
Brett Turing <bjsherman80@gmail.com>
2020-10-20 14:41:09 -0600

    changed variables to make sure test passes

--- spec/features/pets/delete_spec.rb
+++ spec/features/pets/delete_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Pet', type: :feature do
   it 'can delete a pet' do
-    shelter1 = Shelter.create(name: 'Sherms Spikey Friends',
+    shelter2 = Shelter.create(name: 'Phills World',
                                 address: '1489 Balake Ave.',
                                 city: 'Denver',
                                 state: 'CO',
                                 zip: '80201')
-    pet1 = shelter1.pets.create!(name: 'pet1', age: 18, sex: 'male', image: 'vernon.png')
+    pet2 = shelter2.pets.create!(name: 'G', age: 18, sex: 'male', image: 'https://felineengineering.com/wp-content/uploads/2019/07/Adorable-sad-kitten-e1562788887775-974x1024.jpg' )
 
-    visit "/pets/#{pet1.id}"
-    click_on 'Delete'
-    expect(page).to_not have_content('Vernon')
+    visit "/pets/#{pet2.id}"
+      click_on 'Delete'
+    expect(page).to_not have_content('G')
   end
 end
