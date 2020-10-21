7c7575750118375a93c7f1fdae21464d5b1cd592
Brett Turing <bjsherman80@gmail.com>
2020-10-21 14:44:05 -0600

    removed old code

--- app/models/application.rb
+++ app/models/application.rb
@@ -3,15 +3,4 @@ class Application < ApplicationRecord
   belongs_to :user
   has_many :pet_applications
   has_many :pets, through: :pet_applications
-
-    # def update_app_status
-    #   if pets.any?{|pet| pet.status == "Rejected"}
-    #     self.update(status: "Rejected")
-    #   elsif pets.all{|pet| pet.status == "Approved"}
-    #     self.update(status: "Approved")
-    #   else
-    #     self.update(status: "In Progress")
-    #   end
-    # end
-
 end
