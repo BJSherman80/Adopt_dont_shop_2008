333a7624c7a14e572bf14f591dd3debbd5dcbd8e
Brett Turing <bjsherman80@gmail.com>
2020-10-21 14:30:50 -0600

    created model methods for average_shelter_rating and count_of_pet_applications

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,5 +1,15 @@
 class Shelter < ApplicationRecord
   has_many :pets, dependent: :destroy
-  has_many :reviews, dependent: :destroy 
+  has_many :reviews, dependent: :destroy
+  has_many :applications, through: :pets
   validates_presence_of :name, :address, :city, :state, :zip
+
+  def average_shelter_rating
+    reviews.average(:rating)
+  end
+
+  def count_of_pet_applications
+    applications.count
+  end
+
 end
